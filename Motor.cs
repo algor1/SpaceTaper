ad8c9c42a9b49bf37fbc9c7a8a447de510ba92cd
algor1 <algor1@users.noreply.github.com>
2019-08-27 22:33:31 +0300

    Add files via upload

---
+++ Motor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class Motor : MonoBehaviour
+{
+    public float Health{get;private set;}
+    public float Hit {get;set;}
+
+    public void Damage(float hitPoints)
+    {
+        Health-= hitPoints;
+        if (Health<=0 ) 
+        {
+            
+        }
+    }
+
+    public void Delete()
+    {
+        EventHandler<DestroyedEventArgs> handler = Destroed;
+        handler?.Invoke(this, new DestroyedEventArgs {HitPoints= Hit});
+        Destroy(gameObject);
+    }
+
+    public event EventHandler<DestroyedEventArgs> Destroed;
+}
+public class DestroyedEventArgs : EventArgs
+{
+    public float HitPoints{get;set;}
+}
